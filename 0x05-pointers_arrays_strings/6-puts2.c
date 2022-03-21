e115e52043e31f90ad77a0d9f00105e136eb0e91
Lema kefyalew <lmmbiz2002@gmail.com>
2022-03-21 10:52:28 +0300

    Reverse other characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - Prints out every other char of the string.
- * @str: input string to print.
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i))
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
+		_putchar(*(str + i));
+		i = i + 2;
 	}
 	_putchar('\n');
 }
